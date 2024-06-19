ad417ac3e3879dddddcfba9f06003fef6d6a9255
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-19 16:04:37 +0300

    first init

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var birthday: UITextField!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelBirthday: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let savedName = UserDefaults.standard.object(forKey: "name")
+        let savedBDay = UserDefaults.standard.object(forKey: "birthday")
+        
+        // casting with as? vs as!
+        if let newName = savedName as? String {
+            labelName.text = "Name: \(newName)"
+        }
+        
+        if let newBDay = savedBDay as? String {
+            labelBirthday.text = "Birthday: \(newBDay)"
+        }
+        
     }
 
-
+    @IBAction func saveBDay(_ sender: Any) {
+        
+        // to store small data
+        UserDefaults.standard.set(name.text, forKey: "name")
+        UserDefaults.standard.set(birthday.text, forKey: "birthday")
+        // UserDefaults.standard.synchronize()  -> no need to use
+        
+        labelName.text = "Name: \(name.text!)"
+        labelBirthday.text = "Birthday: \(birthday.text!)"
+    }
+    
+    
+    @IBAction func deleteBDay(_ sender: Any) {
+        
+        let savedName = UserDefaults.standard.object(forKey: "name")
+        let savedBDay = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = savedName as? String {
+            UserDefaults.standard.removeObject(forKey: "name")
+            labelName.text = "Name: "
+        }
+        
+        if let newBDay = savedBDay as? String {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            labelBirthday.text = "Birthday: "
+        }
+        
+        
+        
+    }
+    
 }
 
